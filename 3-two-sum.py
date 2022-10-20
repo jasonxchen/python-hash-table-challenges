fd72b222a57b3446f71793c59a21171730786b92
JasonXChen <jasonxchen888@gmail.com>
2022-10-20 19:50:53 -0400

    Simplify logic

--- 3-two-sum.py
+++ 3-two-sum.py
@@ -28,11 +28,11 @@ Output: [0,1]
 def two_sum(nums, target):
   table = {}
   for i in range(len(nums)):
-    if nums[i] not in table:
+    if target - nums[i] not in table:
       table[nums[i]] = i
-    if target - nums[i] in table and table[target - nums[i]] != i:
+    else:
       return f"[{table[target - nums[i]]},{i}]"
 
 print(two_sum([2,7,11,15], 9))    # expect [0,1]
 print(two_sum([3,2,4], 6))        # expect [1,2]
-print(two_sum([3,3], 6))          # expect [0, 1]]
+print(two_sum([3,3], 6))          # expect [0,1]
